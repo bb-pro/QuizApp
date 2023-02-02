d5184519d7eeda4de0a31d74d958c7c1e6e3896a
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-02 15:49:04 +0500

    work with quiz model

---
+++ QuizApp/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Bektemur on 02/02/23.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: [String] = []
+    let correct: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answer = a
+        correct = correctAnswer
+    }
+}
